3353f0f8ce5d5606efecca8c581f1d2bb9371df5
leorecoa <leorecoa2@gmail.com>
2025-11-12 02:53:06 -0300

    O que foi feito: Renderização Correta: Substituí os divs de texto pelos componentes de tela correspondentes: SentenceEditorScreen para a tela de símbolos. TextToSpeechScreen para a tela de texto. TherapistScreen para a tela do acompanhante. Com esta alteração, seu aplicativo voltará a renderizar a interface principal corretamente.

--- components/modals/SettingsModal.tsx
+++ components/modals/SettingsModal.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
-import Icon from '../common/Icon';
+import Icon from '../common/Icon'; // Mantido
 import { BackupData, VoiceSettings, AppearanceSettings, FontSize } from '../../types';
 
 interface SettingsModalProps {
