62ad5a5537650c3c43862e3e24bf9886c753e207
leorecoa <leorecoa2@gmail.com>
2025-11-13 01:06:23 -0300

    atualize o arquivo .eslintrc.js (ou .eslintrc.json) na raiz do seu projeto. A configuração abaixo é um bom ponto de partida para validar os caminhos de importação.

--- components/common/Toast.tsx
+++ components/common/Toast.tsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from 'react';
+{
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true
+  }
+}
 import Icon from './Icon';
 
 interface ToastProps {
